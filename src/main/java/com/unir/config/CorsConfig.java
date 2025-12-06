020e49e110cfb8b3bfd272a4957a32237f52df92
adolfogarcia66 <154864073+adolfogarcia66@users.noreply.github.com>
2025-12-05 23:24:57 -0600

    Update CorsConfig.java

--- src/main/java/com/unir/config/CorsConfig.java
+++ src/main/java/com/unir/config/CorsConfig.java
@@ -18,18 +18,12 @@ public class CorsConfig {
 
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000",
-                "https://biblioteca-front-am1vykfc6-adolfos-projects-d1bf2b18.vercel.app",
-                "biblioteca-front-git-main-adolfos-projects-d1bf2b18.vercel.app"
-));
-        https://biblioteca-front-39lspgxug-adolfos-projects-d1bf2b18.vercel.app/manifest.json
-
+        config.addAllowedOriginPattern("*");
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-
         return source;
     }
 }
+
